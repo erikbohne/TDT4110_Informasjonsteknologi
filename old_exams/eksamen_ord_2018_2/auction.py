ce3eb10cf95ec059dff7136de4ef4a8f15a43302
Erik Bohne <eriknbohne@gmail.com>
2023-12-06 14:44:11 +0100

    old exams

---
+++ old_exams/eksamen_ord_2018_2/auction.py
@@ -0,0 +1,13 @@
+def get_auction_data(filename):
+#code that collects the data, not shown here.
+#fits the examples given in the assignments.
+    return[[ 'Customer','vase','maleri','sykkel','bilbane','tv'],['Per',100  ,0,200   ,0,1500],['Ida',110  ,50 ,200   ,0,1500],['Ottar',200   ,600,200  ,0,1700],['Dag',200   ,600  ,200   ,0,1700],['Lise',400  ,600  ,0,0,0]]
+
+def bid(customer,item,data):
+    column = data[0].index(item)
+    for i in data:
+        if i[0] == customer:
+            return i[column]
+        
+def sort_list(liste,column):
+    return sorted(liste,key=lambda l:l[column], reverse=True)
